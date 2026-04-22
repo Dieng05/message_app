2a78c971cf8d4e9581f114e474e92ad86382e336
l3miage-dossos <siaka.dosso@etu.univ-grenoble-alpes.fr>
2026-04-22 10:51:11 +0200

    welcome page, ok

---
+++ lib/pages/Acceuil.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Acceuil extends StatefulWidget {
+  const Acceuil({super.key});
+
+  @override
+  State<Acceuil> createState() => _AcceuilState();
+}
+
+class _AcceuilState extends State<Acceuil> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 500,
+        centerTitle: true,
+        title: Image(image: AssetImage('assets/images/Accueil.jpeg'),)
+      ),
+
+      body: Column(
+        children: [
+          Center(
+            child:  Text('Message-APP', style: TextStyle(color: Colors.red, fontSize: 40, fontWeight: FontWeight.bold),),
+          ),
+          Text('Restez Connecter parTout'),
+          SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: (){
+
+              },
+                  child: Text('Se Connecter')),
+              ElevatedButton(onPressed: (){
+
+              },
+                  child: Text('Créer Un Compte')),
+            ]
+
+          )
+        ],
+      ),
+    );
+  }
+}
